3fb6449e67bebcf8e113d6b0d44b0ff47033e817
Ian Fitzpatrick <ian.fitzpatrick6@gmail.com>
2018-11-03 18:03:14 +0000

    Initial Commit with Template Files

---
+++ public/js/helpers/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://localhost:8888/';
+export const CLIENT_ID = '';
+export const STATE_KEY = 'spotify_auth_state';
---
+++ public/js/pages/battle.js
@@ -0,0 +1,35 @@
+import SpotifyAPI from "../api/spotify.js";
+import {getHashParams} from "../helpers/url.js";
+import {STATE_KEY} from "../helpers/constants.js";
+
+const USER_PROFILE = document.getElementById('user-profile');
+const {access_token, state} = getHashParams();
+const storedState = localStorage.getItem(STATE_KEY);
+
+
+const outputTemplate = ({display_name, id, email, uri, external_urls, images, country}) =>`<h1>Logged in as </h1>
+  <div class="media">
+    <div class="pull-left">
+      <img class="media-object" width="150" src="">
+    </div>
+    <div class="media-body">
+      <dl class="dl-horizontal">
+        <dt>Display name</dt><dd class="clearfix">${display_name}</dd>
+        <dt>Id</dt><dd>${id}</dd>
+        <dt>Email</dt><dd>${email}</dd>
+        <dt>Spotify URI</dt><dd><a href="${uri}">${uri}</a></dd>
+        <dt>Link</dt><dd><a href="${external_urls.spotify}">${external_urls.spotify}</a></dd>
+        <dt>Profile Image</dt><dd class="clearfix"><a href=""></a></dd>
+        <dt>Country</dt><dd>${country}</dd>
+      </dl>
+    </div>
+  </div>`
+
+
+if (!access_token || (state == null || state !== storedState)) {
+  window.location = "/";
+} else {
+  SpotifyAPI.getUserData(access_token).then(data => {
+    USER_PROFILE.innerHTML = outputTemplate(data);
+  });
+}
