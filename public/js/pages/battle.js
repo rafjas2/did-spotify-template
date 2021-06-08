7261b355844a9d6b8facb97c52eca87e1e460f6c
rafjas2 <rafal_jasinski@yahoo.ie>
2021-06-08 16:20:55 +0100

    some api changis

--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -1,8 +1,8 @@
 import SpotifyAPI from "../api/spotify.js";
-import {getHashParams} from "../helpers/url.js";
-import {STATE_KEY} from "../helpers/constants.js";
+import { getHashParams } from "../helpers/url.js";
+import { STATE_KEY } from "../helpers/constants.js";
 
-const {access_token, state} = getHashParams();
+const { access_token, state } = getHashParams();
 const storedState = localStorage.getItem(STATE_KEY);
 
 if (!access_token || (state == null || state !== storedState)) {
@@ -22,9 +22,9 @@ const searchArtistOne = document.getElementById('search-box-one');
 getArtistOne.addEventListener('click', async () => {
   const artistOne = document.getElementById('search-txt-one').value;
   artist1 = await SpotifyAPI.getArtist(access_token, artistOne);
-  artistImageOne.src = artist1.images[0].url;
+  artistImageOne.src = artist1.images.url;
   artistImageOne.style.border = "3px solid #ffffff";
-  artistNameOne.innerHTML = artist1.name; 
+  artistNameOne.innerHTML = artist1.name;
   searchArtistOne.reset();
   searchArtistOne.style.display = "none";
   console.log("Artist One is: " + artist1.name + "," + " Followers: " + artist1.followers.total);
@@ -36,11 +36,11 @@ const getArtistTwo = document.getElementById('search-btn-two');
 const artistImageTwo = document.querySelector("#artist-two img");
 const artistNameTwo = document.querySelector("#artist-two .artist-name");
 const searchArtistTwo = document.getElementById('search-box-two');
- 
+
 getArtistTwo.addEventListener('click', async () => {
   const artistTwo = document.getElementById('search-txt-two').value;
   artist2 = await SpotifyAPI.getArtist(access_token, artistTwo);
-  artistImageTwo.src = artist2.images[0].url;
+  artistImageTwo.src = artist2.images.url;
   artistImageTwo.style.border = "3px solid #ffffff";
   artistNameTwo.innerHTML = artist2.name;
   searchArtistTwo.reset();
@@ -57,23 +57,23 @@ const winnerName = document.querySelector('#winner .artist-name');
 const winnerImage = document.querySelector("#winner img");
 const winnerFallowers = document.querySelector('#winner .fallowers');
 const logo = document.getElementById('logo');
- 
+
 getBattle.addEventListener('click', () => {
-  if(artist1 === null || artist2 === null) {
-    swal({title: "Please Add The Artists"});
+  if (artist1 === null || artist2 === null) {
+    swal({ title: "Please Add The Artists" });
     return;
   }
 
-  if(artist1.followers.total > artist2.followers.total) {
+  if (artist1.followers.total > artist2.followers.total) {
     winnerImage.src = artist1.images[0].url;
     winnerName.innerHTML = artist1.name;
     winnerFallowers.innerHTML = artist1.followers.total;
-    console.log("Winner is: " + artist1.name + "," + " Followers: " + artist1.followers.total); 
+    console.log("Winner is: " + artist1.name + "," + " Followers: " + artist1.followers.total);
   }
   else {
     winnerImage.src = artist2.images[0].url;
     winnerName.innerHTML = artist2.name;
-    winnerFallowers.innerHTML = artist2.followers.total; 
+    winnerFallowers.innerHTML = artist2.followers.total;
     console.log("Winner is: " + artist2.name + "," + " Followers: " + artist2.followers.total);
   }
 
@@ -82,15 +82,15 @@ getBattle.addEventListener('click', () => {
   backToBattle.style.display = "block";
   logo.style.display = "none";
   winner.style.display = "block";
-  artistImageOne.src = "../img/spartan-one.png"; 
+  artistImageOne.src = "../img/spartan-one.png";
   artistImageOne.style.border = "none";
   artistImageTwo.src = "../img/spartan-two.png";
   artistImageTwo.style.border = "none";
   artistNameOne.innerHTML = "";
-  artistNameTwo.innerHTML = "";  
+  artistNameTwo.innerHTML = "";
   artist1 = null;
   artist2 = null;
-  
+
 });
 
 /*=================== Back To The Buttle Again ======================*/
